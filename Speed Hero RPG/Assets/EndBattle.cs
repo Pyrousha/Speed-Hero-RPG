6eb59c2b9a91653a8b8e2d0caada8bb1a5daa8ea
Travis <tbird3@wisc.edu>
2021-03-10 02:05:29 -0600

    Combat scene transition implemented (and damn well I'd say too wowie)

---
+++ Speed Hero RPG/Assets/EndBattle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndBattle : MonoBehaviour
+{
+    public BattleTransition persistObj;
+    public GameObject battleObjParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        persistObj = GameObject.Find("PersistentGameInfo").GetComponent<BattleTransition>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            EndBattleScene();
+        }      
+    }
+
+    public void EndBattleScene()
+    {
+        persistObj.battleObjParent = battleObjParent;
+        persistObj.TransitionFromBattle();
+    }
+}
