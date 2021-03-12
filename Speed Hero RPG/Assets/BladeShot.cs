89aa618607a378d534c13f2cecb309f09e8472ef
Travis <tbird3@wisc.edu>
2021-03-11 19:45:34 -0600

    23579 attack animations

---
+++ Speed Hero RPG/Assets/BladeShot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeShot : MonoBehaviour
+{
+    Rigidbody2D RB;
+    public float initialSpeed;
+    public float decellerateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        RB.velocity = RB.velocity * decellerateSpeed;
+    }
+
+    public void Destroy()
+    {
+        RB.velocity = new Vector2(0, 0);
+        Destroy(gameObject);
+    }
+}
---
+++ Speed Hero RPG/Assets/SpawnAttack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAttack : MonoBehaviour
+{
+    public ShotCreator shotCreator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void atk_2()
+    {
+        shotCreator.attack_2();
+    }
+    void atk_3()
+    {
+        shotCreator.attack_3();
+    }
+    void atk_5()
+    {
+        shotCreator.attack_5();
+    }
+    void atk_7()
+    {
+        shotCreator.attack_7();
+    }
+    void atk_9()
+    {
+        shotCreator.attack_9();
+    }
+}
