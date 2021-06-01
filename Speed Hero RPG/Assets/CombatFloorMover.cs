2a63ea3facd1a0646608c03d2a349acb96a0b518
Travis <tbird3@wisc.edu>
2021-06-01 01:34:49 -0500

    Fancy combat background and stars

---
+++ Speed Hero RPG/Assets/CombatFloorMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatFloorMover : MonoBehaviour
+{
+    public GameObject bg_1;
+    public GameObject bg_2;
+
+    public Vector3 move_1;
+    public Vector3 move_2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bg_1.transform.position += move_1 * Time.deltaTime;
+        if (bg_1.transform.localPosition.x > 1)
+            bg_1.transform.localPosition += new Vector3(-1, 0, 0);
+        
+        bg_2.transform.position += move_2 * Time.deltaTime;
+        if ((bg_2.transform.localPosition.x > 1) && (bg_2.transform.localPosition.y > 1))
+            bg_2.transform.localPosition += new Vector3(-1, -1, 0);
+    }
+}
