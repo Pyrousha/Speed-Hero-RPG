f815a4246261057bfddd6cae179d478e74d5ee66
Travis <tbird3@wisc.edu>
2021-07-27 16:56:50 -0500

    Hero sprite now properly under water when falling in

---
+++ Assets/ChangeHeroLayerTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeHeroLayerTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Hero Hitbox")
+        {
+            collision.transform.GetChild(2).GetComponent<SpriteRenderer>().sortingOrder = -2;
+        }
+    }
+}
