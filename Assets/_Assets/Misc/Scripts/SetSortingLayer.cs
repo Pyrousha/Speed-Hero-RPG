0d915631ca6092bacdb29f1f426ea49dc2495f4d
Travis <tbird3@wisc.edu>
2021-10-10 21:40:42 -0500

    "Telegraph" for enemy attacks (Seizure warning??)

--- Assets/_Assets/Misc/Scripts/SetSortingLayer.cs
+++ Assets/_Assets/Misc/Scripts/SetSortingLayer.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[ExecuteInEditMode]
+//[ExecuteInEditMode]
 public class SetSortingLayer : MonoBehaviour
 {
     public Renderer MyRenderer;
