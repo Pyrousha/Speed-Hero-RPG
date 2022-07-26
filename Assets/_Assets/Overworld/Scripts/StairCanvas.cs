5a1676451a4dcf47798e89b900452e5d683f2c3f
travisbird0 <tboydlink@gmail.com>
2022-07-26 10:01:09 -0500

    cave1 lighting fixed, stair walk anims now sync with player

--- Assets/_Assets/Overworld/Scripts/StairCanvas.cs
+++ Assets/_Assets/Overworld/Scripts/StairCanvas.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class StairCanvas : Singleton<StairCanvas>
 {
+    [SerializeField] private Animator heroSpriteAnim;
     [SerializeField] private Transform heroTransform;
+
     [SerializeField] private Animator anim;
+    [SerializeField] private Animator walkAnim;
     private Vector3 targPosition;
 
     public void TriggerHit(StairParent.Dir direction, Vector3 newTargPos)
@@ -28,5 +31,17 @@ public class StairCanvas : Singleton<StairCanvas>
     public void Warp()
     {
         heroTransform.localPosition = targPosition;
+    }
+
+    public void PlayWalkAnim(StairParent.Dir direction)
+    {
+        if (direction == StairParent.Dir.left)
+        {
+            walkAnim.Play("stair_walk_left", 0, heroSpriteAnim.GetCurrentAnimatorStateInfo(0).normalizedTime);
+        }
+        else
+        {
+            walkAnim.Play("stair_walk_right", 0, heroSpriteAnim.GetCurrentAnimatorStateInfo(0).normalizedTime);
+        }
     }
 }
